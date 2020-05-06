ae413df1cd5554029349d797999dcdf94ccacc2b
Kazuya <shiozaki5621@gmail.com>
2020-05-06 23:53:26 +0900

    add three_layer_NN

---
+++ chapter3/three_layer_NN.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.5]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+
+def sigmoid_function(x):
+    return 1 / (1 + np.exp(x))
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid_function(a1)
+    a2 = np.dot(z1, W2) + b2
+
+
+
