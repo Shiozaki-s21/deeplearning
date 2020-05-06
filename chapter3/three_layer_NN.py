dd36978c805ed1933051d365845b1ef1ff93dd69
Kazuya <shiozaki5621@gmail.com>
2020-05-07 01:05:27 +0900

    fix three_layer_NN

--- chapter3/three_layer_NN.py
+++ chapter3/three_layer_NN.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 
+def identity_function(x):
+    return x
+
 def init_network():
     network = {}
     network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
     network['b1'] = np.array([0.1, 0.2, 0.3])
     network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
     network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.5]])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
     network['b3'] = np.array([0.1, 0.2])
 
     return network
@@ -24,6 +27,13 @@ def forward(network, x):
     a1 = np.dot(x, W1) + b1
     z1 = sigmoid_function(a1)
     a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid_function(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
 
+    return y
 
-
+network  = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
