bd514b3a70908a1a9e0b4b7c65da8ffbe29dd369
Kazuya <shiozaki5621@gmail.com>
2020-06-13 15:44:27 +0900

    add train_numeric

---
+++ chapter5/train_numeric.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+from chapter5.two_layer_net import TwoLayerNet
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
