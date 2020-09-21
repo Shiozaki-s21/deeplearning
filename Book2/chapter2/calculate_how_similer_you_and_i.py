36cd0b778a230deb77a01712999e73d77f2ef52f
Kazuya <shiozaki5621@gmail.com>
2020-09-21 18:06:46 +0900

    add util

--- Book2/chapter2/calculate_how_similer_you_and_i.py
+++ Book2/chapter2/calculate_how_similer_you_and_i.py
@@ -1,9 +1,9 @@
 # import sys
 # from Book2.chapter2 import preprocess, create_co_matrix, cos_similarity
 # sys.path.append('')
-from Book2.chapter2.preprocess import preprocess
+from Book2.common.utill import preprocess, cos_similarity, create_to_matrix
 
 text: str = 'You say goodby and I say hallo'
 corpus, word_to_id, id_to_word = preprocess(text)
 
-print(corpus)
+
---
+++ Book2/common/util.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def preprocess(text:str):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    # corpus is a list of word ID
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
+
+
+def create_to_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - 1
+            right_idx = idx + 1
+
+            # check for out of index
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            # check for out of index
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+
+def cos_similarity(x, y, eps=1e-8):
+    # normalization
+    nx = x / (np.sqrt(np.sum(x**2)) + eps)
+    ny = x / np.sqrt((np.sum(y**2)) + eps)
+
+    # after normalization, calculate scalar
+    return np.dot(nx, ny)
+
