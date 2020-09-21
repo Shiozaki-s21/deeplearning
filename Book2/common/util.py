0237791bb302dd24270f0259e05d73e021018739
Kazuya <shiozaki5621@gmail.com>
2020-09-22 03:11:45 +0900

    add use ppmi

--- Book2/common/util.py
+++ Book2/common/util.py
@@ -51,3 +51,22 @@ def cos_similarity(x, y, eps=1e-8):
     # after normalization, calculate scalar
     return np.dot(nx, ny)
 
+
+def ppmi(C, verbose=False, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j] * S[j]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100) == 0:
+                    print('%.1f%% done' % (100 * cnt/total))
+
+    return M
